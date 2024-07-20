714c60aab536a3f1e5ee1fa5d7d38f5014ac2b93
sudo-chmod <chamodhhenderson@gmail.com>
2024-07-20 14:23:05 +0530

    Update my_client_app.java

--- src/my_client_app.java
+++ src/my_client_app.java
@@ -5,19 +5,28 @@ import java.net.*;
 
 public class my_client_app {
     public static void main(String[] args) {
-        //java my_client_app.java <server ip> <port>
+        //java my_client_app.java <hostname> <port>
         String hostname = args[0];
         int port = Integer.parseInt(args[1]);
-        try (Socket socket = new Socket(hostname, port);
-             OutputStream output = socket.getOutputStream();
-             PrintWriter writer = new PrintWriter(output, true);
-             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+
+        try {
+            Socket socket = new Socket(hostname, port);
+
+            PrintWriter sender = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("Connection Established");
             String message;
             do {
                 System.out.print("Enter message: ");
                 message = reader.readLine();
-                writer.println(message);
+                sender.println(message);
             } while (!message.equalsIgnoreCase("terminate"));
+
+            reader.close();
+            sender.close();
+            socket.close();
+            System.out.println("Connection Closed");
         } catch (UnknownHostException e) {
             System.out.println("my_server_app not found: " + e.getMessage());
         } catch (IOException e) {
